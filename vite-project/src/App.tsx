c02d76d1f1b3cffb98ab964a1945a33d5b597439
AxterDoesCode <74308086+AxterDoesCode@users.noreply.github.com>
2022-08-15 20:37:40 +0100

    started laying out the page and changed routing

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,15 +2,27 @@ import Home from './Home'
 import About from "./About"
 import Projects from "./Projects"
 import Contact from "./Contact"
-import {Routes, Route} from "react-router-dom"
+import {Link, Routes, Route} from "react-router-dom"
 function App() {
   return(
-    <Routes>
-      <Route path="/*" element={<Home />} />
-      <Route path="about" element={<About />}/>
-      <Route path="projects" element={<Projects />}/>
-      <Route path="contact" element={<Contact />}/>
-    </Routes>
+    <>
+      <header>
+        <nav className="nav">
+          <Link to="/" className="nav--link">Home</Link>
+          <Link to="about" className="nav--link">About</Link>
+          <Link to="projects" className="nav--link">Projects</Link>
+          <Link to="contact" className="nav--link">Contact</Link>  
+        </nav>
+        <p className='name'>alex.chau</p>
+      </header>
+
+      <Routes>
+        <Route path="/*" element={<Home />} />
+        <Route path="about" element={<About />}/>
+        <Route path="projects" element={<Projects />}/>
+        <Route path="contact" element={<Contact />}/>
+      </Routes>
+    </>
   )
 }
 
--- vite-project/src/Home.tsx
+++ vite-project/src/Home.tsx
@@ -1,11 +1,9 @@
 import { Link } from "react-router-dom"
 export default function Home (){
     return(
-        <div className="Home">
-            <h1 className="large"><Link to="about">Hello</Link></h1>
-            <h1 className="large red"><Link to="projects">I am</Link></h1>
-            <h1 className="large red"><Link to="contact">Alex</Link></h1>
-
-        </div>
+        <>
+            <h1>Hello</h1>
+            <h2>Hello</h2>   
+        </>
     )
 }
