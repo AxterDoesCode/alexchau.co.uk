067b967a1cf5e73964ba52a97cbce00f5cbe28b8
AxterDoesCode <74308086+AxterDoesCode@users.noreply.github.com>
2022-08-18 19:28:03 +0100

    Hide scrollbars on Home animation

--- vite-project/src/Components/Home/Home.tsx
+++ vite-project/src/Components/Home/Home.tsx
@@ -1,13 +1,17 @@
 import Title from "./SubComponents/Title"
 import Section from "./SubComponents/Section"
+import styled from "styled-components"
 
+const StyledMain = styled.main`
+    overflow: hidden;
+`
 export default function Home (){
     return(
-        <main>
+        <StyledMain>
             <Section>
                 <Title className="animate-rise">Hello</Title>
                 <Title className="animate-rise delay04">I'm <span className="home--name">Alex</span></Title>
             </Section>
-        </main>
+        </StyledMain>
     )
 }
